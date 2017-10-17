fa24b675fb969be5cc3be38c66f5a54f5c8a6977
Eva <evajohnson714@gmail.com>
2017-10-17 03:16:21 +0300

    insert item cobra to the string

---
+++ index2.py
@@ -0,0 +1,8 @@
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = 2# Use index() to find "duck"
+
+# Your code here!
+print animals.index("duck")
+animals.insert(duck_index, "cobra")
+
+print animals # Observe what prints after the insert operation
