074b36c11d5e037e4b29a180e30da8963845a3fd
Eva <evajohnson714@gmail.com>
2017-10-17 04:20:42 +0300

    Add the new key value pairs to the dictionary

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
-menu = {'Chicken Alfredo':14.50,'chips':50.50,'meat':70.5,'pizza':800} # Empty dictionary
+menu = {} # Empty dictionary
 menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
 print menu['Chicken Alfredo']
 
 # Your code here: Add some dish-price pairs to menu!
-
-
+menu['chips'] = 50.50
+menu['meat'] = 70.50
+menu['pizza'] =800
 
 
 print "There are " + str(len(menu)) + " items on the menu."
