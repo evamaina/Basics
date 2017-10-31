a0ee9d4d3a4d58932fbc6e880aff9e4685cd4aa0
Eva <evajohnson714@gmail.com>
2017-10-31 15:11:52 +0300

    printing out thekeys,items and values separately in the dictionery

---
+++ my_dict2.py
@@ -0,0 +1,15 @@
+my_dict = {
+  'name': 'Nick',
+  'age':  31,
+  'occupation': 'Dentist',
+}
+
+print my_dict.items()
+print my_dict.keys()
+print my_dict.values()
+
+
+"""While .items() returns an array of tuples with each tuple consisting of a key/value pair from the dictionary:
+
+The .keys() method returns a list of the dictionary's keys, and
+The .values() method returns a list of the dictionary's values."""
