036e50c0e1df91180bab3f8d6201559fd7b0a266
Eva <evajohnson714@gmail.com>
2017-10-31 15:13:48 +0300

    Using for loop to print items in the dictionery

---
+++ my_dict3.py
@@ -0,0 +1,9 @@
+my_dict = {
+  'name': 'Nick',
+  'age':  31,
+  'occupation': 'Dentist',
+}
+for key in my_dict:
+  print key,my_dict[key]
+
+  #Note that the trailing comma ensures that we keep printing on the same line
